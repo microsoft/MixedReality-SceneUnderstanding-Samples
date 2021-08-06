44dd28a47d4d880496a56087ba7acb7c2d879d4b
jorgonz@microsoft.com <jorgonz@microsoft.com>
2021-08-05 19:26:07 -0700

    Checkpoint 2

--- Assets/SceneUnderstanding/Examples/Placement/Scripts/SceneUnderstandingObjectPlacer.cs
+++ Assets/SceneUnderstanding/Examples/Placement/Scripts/SceneUnderstandingObjectPlacer.cs
@@ -15,9 +15,11 @@ namespace Microsoft.MixedReality.SceneUnderstanding.Samples.Unity
     {
         public GameObject objToPlaceRef;
         public Material material;
-
+        public bool UseSUSDKPlacementAPI;
+        public SceneUnderstandingManager manager;
         private GameObject objToPlace = null;
         private bool isPlacing = false;
+        private readonly float placementScaleForSUSDKAPI = 0.125f;
 
         // Container for all instantiated objects/holograms
         private List<GameObject> holoObjects =  new List<GameObject>();
@@ -29,44 +31,14 @@ namespace Microsoft.MixedReality.SceneUnderstanding.Samples.Unity
             // Add object to the list
             holoObjects.Add(objToPlace);
 
-            // Disable collider for base object if it has any
-            Collider parentCollider = objToPlace.GetComponent<Collider>();
-            if(parentCollider != null)
-            {
-                parentCollider.enabled = false;
-            }
-
-            // Disable colliders for any child objects if any exists
-            foreach(Transform child in objToPlace.transform)
-            {
-                Collider childCollider = child.GetComponent<Collider>();
-
-                if(childCollider != null)
-                {
-                    childCollider.enabled = false;
-                }
-            }
+            // Disable colliders for base object and children if it has any
+            RecursiveToggleColliders(objToPlace, false);
         }
 
         private void FinishPlacing()
         {
-            // Enable collider for base object if it has any
-            Collider parentCollider = objToPlace.GetComponent<Collider>();
-            if(parentCollider != null)
-            {
-                parentCollider.enabled = true;
-            }
-
-            // Enable colliders for any child objects if any exists
-            foreach(Transform child in objToPlace.transform)
-            {
-                Collider childCollider = child.GetComponent<Collider>();
-
-                if(childCollider != null)
-                {
-                    childCollider.enabled = true;
-                }
-            }
+            // Enable colliders for base object and children if it has any
+            RecursiveToggleColliders(objToPlace, true);
 
             objToPlace = null;
         }
@@ -78,11 +50,11 @@ namespace Microsoft.MixedReality.SceneUnderstanding.Samples.Unity
                 return;
             }
 
-            Vector3 newObjPosition = GetDesiredObjPos();
+            Vector3 newObjPosition = GetDesiredObjPos(UseSUSDKPlacementAPI);
             objToPlace.transform.position = newObjPosition;
         }
 
-        private Vector3 GetDesiredObjPos()
+        private Vector3 GetDesiredObjPos(bool useCenterMostPlacementAPI = false)
         {
             RaycastHit hit;
             bool hasTarget = Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit);
@@ -90,9 +62,29 @@ namespace Microsoft.MixedReality.SceneUnderstanding.Samples.Unity
             Vector3 newObjPos = Vector3.zero;
             if(hasTarget)
             {
-                // Get a Position Slightly above of the object that the main camera is gazing at
-                Vector3 selectedObjFacingTowards = -hit.transform.forward.normalized;
-                newObjPos = Vector3.Dot(Camera.main.transform.TransformDirection(Vector3.forward), selectedObjFacingTowards) < 0 ? hit.point + (selectedObjFacingTowards * 0.3f) : hit.point - (selectedObjFacingTowards * 0.3f);
+                if(useCenterMostPlacementAPI)
+                {
+                    // Use the SU SDK placement API to get a valid position
+                    Vector3? result = GetPositionFromSUSDKAPI(hit.transform);
+
+                    if(result != null)
+                    {
+                        Vector3 selectedObjFacingTowards = -hit.transform.forward.normalized;
+                        newObjPos = Vector3.Dot(Camera.main.transform.TransformDirection(Vector3.forward), selectedObjFacingTowards) < 0 ? result.Value + (selectedObjFacingTowards * 0.3f) : result.Value - (selectedObjFacingTowards * 0.3f);
+                    }
+                    else
+                    {
+                        // If no object is being gazed at, then place the object infront of the camera.
+                        newObjPos = Camera.main.transform.position + (Camera.main.transform.forward * 2.0f);
+                    }
+                }
+                else
+                {
+                    // Use Unity to get a valid position
+                    // Get a Position Slightly above of the object that the main camera is gazing at
+                    Vector3 selectedObjFacingTowards = -hit.transform.forward.normalized;
+                    newObjPos = Vector3.Dot(Camera.main.transform.TransformDirection(Vector3.forward), selectedObjFacingTowards) < 0 ? hit.point + (selectedObjFacingTowards * 0.3f) : hit.point - (selectedObjFacingTowards * 0.3f);
+                }
             }
             else
             {
@@ -125,6 +117,110 @@ namespace Microsoft.MixedReality.SceneUnderstanding.Samples.Unity
                 holoObjects.Add(tempgbj);
 
                 yield return new WaitForSeconds(0.1f);
+            }
+        }
+
+        private void RecursiveToggleColliders(GameObject root, bool enableColliders = false)
+        {
+            if(root != null)
+            {
+                foreach(Collider col in root.GetComponents<Collider>())
+                {
+                    col.enabled = enableColliders;
+                }
+
+                foreach(Transform child in root.transform)
+                {
+                    RecursiveToggleColliders(child.gameObject, enableColliders);
+                }
+            }
+        }
+
+        private Vector3? GetPositionFromSUSDKAPI(Transform obj)
+        {
+            Scene suScene = manager.GetLatestDeserializedScene();
+            if(suScene == null)
+            {
+                return null;
+            }
+
+            SceneUnderstandingProperties properties = obj.transform.parent.GetComponent<SceneUnderstandingProperties>();
+            if(properties == null)
+            {
+                return null;
+            }
+
+            SceneObject suObject = (SceneObject) suScene.FindComponent(properties.suObjectGUID);
+            if(suObject == null)
+            {
+                return null;
+            }
+
+            // Before starting to find the proper location, calculate
+            // the volume of the object we want to place
+            Mesh mesh = objToPlace.GetComponent<MeshFilter>().mesh;
+            System.Numerics.Vector2 objectExtent = new System.Numerics.Vector2(mesh.bounds.size.x, mesh.bounds.size.z) * placementScaleForSUSDKAPI;
+
+            //Grab the largest quad, it's very likely that only one quad exists
+            //but let's be thorough
+            SceneQuad largestQuad = null;
+            float fLargestArea = float.NegativeInfinity;
+            foreach (SceneQuad quad in suObject.Quads)
+            {
+                System.Numerics.Vector2 extents = quad.Extents;
+                float area = extents.X * extents.Y;
+
+                if (area > fLargestArea)
+                {
+                    fLargestArea = area;
+                    largestQuad = quad;
+                }
+            }
+
+            if (largestQuad == null)
+            {
+                //No largest Quad could be calculated
+                return null;
+            }
+
+            //Use the 'FindCenterMostPlacement' API from the SU SDK to calculate a location on the surface
+            System.Numerics.Vector2 result;
+            if (largestQuad.FindCentermostPlacement(objectExtent, out result))
+            {
+                //Now we have the CenterMostPlacement for this Quad Object, but the Coordinates from the 'FindCenterMostPlacement'
+                //API align to 'Quad Space' where the origin is in the top left of the Quad. 
+
+                //Create a pivot gameobject, we need to start moving in local space relative to the coordinates of the surface
+                //it's easier to anchor a child gameobject to the origin and then move this gameobject to the desired location
+                //since it already starts in the local origin of the surface
+                GameObject location = new GameObject("CenterOfPlatform");
+                location.transform.parent = obj.transform;
+                location.transform.localPosition = Vector3.zero;
+                location.transform.localRotation = Quaternion.identity;
+
+                //Our pivot gameobject starts in the middle of the platform, that's the local origin in Unity, but we need to move it to the top left 
+                //corner of the object. 'FindCenterMostPlacement' coordinates work in 'Quad Space'
+                //they start at the top left of the Quad, and go down and right correspondingly in X and Y axis
+
+                //Move to the starting position aka. Top Left Corner
+                location.transform.Translate(-(largestQuad.Extents.X / 2.0f), largestQuad.Extents.Y / 2.0f, 0.0f, Space.Self);
+
+                //Move to the CenterMost Place -- Y is negate so that we move right and down, according to the API 'FindCenterMostPlacement'
+                //https://docs.microsoft.com/en-us/windows/mixed-reality/develop/platform-capabilities-and-apis/scene-understanding-SDK#quad
+                location.transform.Translate(result.X, -result.Y, 0.0f, Space.Self);
+
+                //Store the postion for plaement
+                Vector3 resultPos = location.transform.position;
+
+                //We can delete the pivot now, we don't need it anymore
+                Destroy(location);
+
+                return resultPos;
+            }
+            else
+            {
+                //Object is to small to place this object
+                return null;
             }
         }
 
